faabd2b7729f1d57c89278bf8e037e0a54379328
BriseydaArbizuValladolid <briseyda.arbizu.valladolid@vallegrande.edu.pe>
2026-04-22 19:15:13 -0500

    feat: update dockerfile

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/v1': {
-        target: 'http://localhost:8082',
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     }
